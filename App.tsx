5860edcc2043e3662893d622197fff26204c1187
Asme Mussie <asmebold@gmail.com>
2023-09-19 10:23:12 +0100

    adds signup and login

--- App.tsx
+++ App.tsx
@@ -1,67 +1,39 @@
-import { StyleSheet, Button, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
 
-import Home from "./screens/Home";
-import Discover from "./screens/Discover";
+const Stack = createNativeStackNavigator();
 
-const Tab = createBottomTabNavigator();
+const App: React.FC = () => {
+  const [user, setUser] = useState(null);
 
-export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
+      <Stack.Navigator
         screenOptions={{
-          headerTitle: "Book Club",
-          headerStyle: {
-            backgroundColor: "darkblue",
-          },
-          headerTintColor: "white",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
+          headerTitle: "Bookclub",
+          headerTitleAlign: "center",
         }}
       >
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerRight: () => (
-              <Button
-                onPress={() => alert("This will take you to profile")}
-                title="Profile pic"
-                color="#fff"
-              />
-            ),
-          }}
-        />
-
-<Tab.Screen
-          name="Discover"
-          component={Discover}
-          options={{
-            headerRight: () => (
-              <Button
-                onPress={() => alert("This will take you to profile")}
-                title="Profile pic"
-                color="#fff"
-              />
-            ),
-          }}
-        />
-
-
-
-      </Tab.Navigator>
+        {user ? (
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: "Bookclub" }}
+          />
+        ) : (
+          <>
+            <Stack.Screen name="Sign up" component={SignupScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </>
+        )}
+      </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from "react";
+import {
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useNavigation } from "@react-navigation/native";
+import { auth } from "../firebase-config";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+interface LoginScreenProps {
+  navigation: any;
+}
+
+const LoginScreen: React.FC<LoginScreenProps> = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const goToSignup = () => {
+    navigation.navigate("Sign up");
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigation.replace("Home");
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        if (user) {
+          navigation.replace("Home");
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAwareScrollView
+        style={{ flex: 1, width: "100%" }}
+        keyboardShouldPersistTaps="always"
+      >
+        <Image style={styles.logo} source={require("../../img/logo.webp")} />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaaaaa"
+          secureTextEntry
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity style={styles.button} onPress={() => handleLogin()}>
+          <Text style={styles.buttonTitle}>Login</Text>
+        </TouchableOpacity>
+        <View style={styles.footerView}>
+          <Text style={styles.footerText}>
+            Don't have an account?
+            <Text onPress={goToSignup} style={styles.footerLink}>
+              Sign up
+            </Text>
+          </Text>
+        </View>
+      </KeyboardAwareScrollView>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "blanchedalmond",
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {},
+  logo: {
+    flex: 1,
+    height: 100,
+    width: 120,
+    objectFit: "cover",
+    alignSelf: "center",
+    margin: 40,
+    borderRadius: 10,
+    opacity: 0.8,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "#fff",
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+  },
+  button: {
+    backgroundColor: "#788eec",
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonTitle: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  footerView: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "#2e2e2d",
+  },
+  footerLink: {
+    color: "#788eec",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
